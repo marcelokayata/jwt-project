6c6b3ea685d5d63307cb895742bff0e3415ef343
Marcelo Apovian Kayata <marcelo_kayata@hotmail.com>
2026-04-14 05:11:24 -0300

    feat: add frontend

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = require("./routes/login");
 const app = express();
 
+app.use(express.static("./public"))
 app.use(express.json());
 app.use(router);
 
